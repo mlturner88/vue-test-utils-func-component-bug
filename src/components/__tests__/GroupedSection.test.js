2072bc3d588b5467dbf40ee56a39616a7df47e50
Matt Turner <turnerml@ornl.gov>
2022-05-10 11:58:26 -0400

    added unit test that changed prop after initial render/mount

--- src/components/__tests__/GroupedSection.test.js
+++ src/components/__tests__/GroupedSection.test.js
@@ -1,13 +1,24 @@
-import { mount } from '@vue/test-utils';
-import GroupedSection from '../GroupedSection.js';
+import { mount } from "@vue/test-utils";
+import GroupedSection from "../GroupedSection.js";
 
-test('should render header', () => {
-  const header = 'A Header';
+test("should render header", () => {
+  const header = "A Header";
   const w = mount(GroupedSection, {
-    attrs: { header },
-    slots: { default: '<div>Text Content</div>' }
+    props: { header },
+    slots: { default: "<div>Text Content</div>" },
   });
 
+  // debugging this test, the prop is not populated at all when component is rendered
+  expect(w.html()).toContain(header);
+});
+
+test("should render header after props update", () => {
+  const header = "A Header";
+  const w = mount(GroupedSection, {
+    slots: { default: "<div>Text Content</div>" },
+  });
+
+  w.setProps({ header });
   // debugging this test, the prop is not populated at all when component is rendered
   expect(w.html()).toContain(header);
 });
